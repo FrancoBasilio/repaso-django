5526c685cdde64ab9bc4b6955b7ab1778c51cf05
Franco Basilio <basiliofranco1999@gmail.com>
2022-03-24 19:24:49 -0300

    Agregamos nuestra primer app y sus 2 listas

---
+++ index/urls.py
@@ -0,0 +1,7 @@
+from django.urls import URLPattern, path
+from .views import index, plantilla
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('plantilla/', plantilla, name='plantilla'),
+]
--- repasodjango/settings.py
+++ repasodjango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from operator import index
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'index'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'repasodjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\Users\erago\Desktop\Repaso-Django\index\plantillas'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
